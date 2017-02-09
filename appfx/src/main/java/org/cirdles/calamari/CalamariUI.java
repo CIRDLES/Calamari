4087282f7db3d49317608428894e68c458fc24d5
Jim Bowring <bowring@gmail.com>
2017-02-09 11:00:18 -0500

    refactors appfx

--- appfx/src/main/java/org/cirdles/calamari/CalamariUI.java
+++ appfx/src/main/java/org/cirdles/calamari/CalamariUI.java
@@ -5,24 +5,31 @@
  */
 package org.cirdles.calamari;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+
 import javafx.stage.Stage;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author bowring
  */
-public class Calamari extends Application {
+public class CalamariUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
 
         primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("./images/topSoilModern32.png")));
+
+        URL iconURL = this.getClass().getResource("./images/topSoilModern32.png");
+        java.awt.Image image = new ImageIcon(iconURL).getImage();
+        com.apple.eawt.Application.getApplication().setDockIconImage(image);
 
         Scene scene = new Scene(root);
 
