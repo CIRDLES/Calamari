62e3662beed5b7246efa737cf69244389bba45ed
Jim Bowring <bowring@gmail.com>
2017-03-03 13:54:40 -0500

    wtf

--- appfx/src/main/java/org/cirdles/calamari/FXMLDocumentTestController.java
+++ appfx/src/main/java/org/cirdles/calamari/FXMLDocumentTestController.java
@@ -5,7 +5,10 @@
  */
 package org.cirdles.calamari;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,8 +16,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import org.cirdles.commons.util.ResourceExtractor;
 
 /**
@@ -41,11 +46,74 @@ public class FXMLDocumentTestController implements Initializable {
         ResourceExtractor prawnFileResourceExtractor
                 = new ResourceExtractor(Calamari.class);
 
-        File prawnFileResource = prawnFileResourceExtractor.extractResourceAsFile("./images/SquidLogo.png");
-        File prawnFile = new File("SquidLogo.png");
-        prawnFileResource.renameTo(prawnFile);
-        imageview.setImage(new Image("../../../../../../../../SquidLogo.png"));
+        File exampleFolder = new File("imagesFX");
 
+//        try {this works +++++++++++++++++++
+//            File image = new File(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png");
+//            String imageFileString = image.toURI().toURL().toString();
+//            imageview.setImage(new Image(imageFileString));
+//        } catch (IOException iOException) {
+//        }
+//        try {
+//            if (exampleFolder.exists()) {
+//                FileUtilities.recursiveDelete(exampleFolder.toPath());
+//            }
+//            if (exampleFolder.mkdir()) {
+//                try {
+//                    File prawnFileResource = prawnFileResourceExtractor.extractResourceAsFile("./images/SquidLogo.png");
+//                    String imageFileString = prawnFileResource.toURI().toURL().toString();
+//                    imageview.setImage(new Image(imageFileString));
+//                } catch (MalformedURLException malformedURLException) {
+//                }
+////                File prawnFile = new File(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png");
+////                prawnFileResource.renameTo(prawnFile);
+////
+////                //imageview.setImage(new Image(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png"));
+////                imageview.setImage(new Image(prawnFile.toURI().toString()));
+//            }
+//        } catch (IOException iOException) {
+//        }
+        try {
+//            final ImageReader rdr = ImageIO.getImageReadersByFormatName("PNG").next();
+//            InputStream initialStream = FXMLDocumentTestController.class.getResourceAsStream("./images/3.png");
+//            final ImageInputStream imageInput = ImageIO.createImageInputStream(initialStream);
+//            rdr.setInput(imageInput);
+//            final BufferedImage imBuff = rdr.read(0);
+//            initialStream.close();
+
+
+            InputStream initialStream = FXMLDocumentTestController.class.getResourceAsStream("./images/SquidLogo.png");
+            File prawnFileResource = prawnFileResourceExtractor.extractResourceAsFile("./images/SquidLogo.png");
+            File prawnFile = new File(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png");
+                prawnFileResource.renameTo(prawnFile);
+
+            BufferedImage imBuff = ImageIO.read(prawnFile);
+            File outputFile = new File(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png");
+            ImageIO.write(imBuff, "PNG", outputFile);
+        } catch (IOException iOException) {
+        }
+
+//            byte[] buffer = new byte[initialStream.available()];
+//            initialStream.read(buffer);
+//
+//            File targetFile = new File(exampleFolder.getCanonicalPath() + File.separator + "SquidLogo.png");
+//            Files.write(buffer, targetFile);
+//        BufferedImage image = null;
+//        try {
+//            InputStream fin = FXMLDocumentTestController.class.getResourceAsStream("./images/SquidLogo.png");
+//            byte[] buffer = new byte[initialStream.available()];
+//            initialStream.read(buffer);
+//
+//            File targetFile = new File("src/main/resources/targetFile.tmp");
+//            OutputStream outStream = new FileOutputStream(targetFile);
+//            outStream.write(buffer);
+//
+//            image = ImageIO.read(FXMLDocumentTestController.class.getResourceAsStream("./images/SquidLogo.png"));
+//        } catch (IOException iOException) {
+//        }
+//
+//        javafx.scene.image.WritableImage fxImage = SwingFXUtils.toFXImage(image, null);
+//        imageview.setImage(fxImage);
     }
 
     @FXML
