a713a7bcb02221ff4e91abfa303155d9d5ecb236
jimbowring <sbowring@cirdlesIIMBP580.attlocal.net>
2017-03-31 09:11:24 -0400

    adding support for AgePb76

---
+++ core/src/main/java/org/cirdles/calamari/tasks/expressions/functions/AgePb76.java
@@ -0,0 +1,93 @@
+/* 
+ * Copyright 2006-2017 CIRDLES.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cirdles.calamari.tasks.expressions.functions;
+
+import java.util.List;
+import org.cirdles.calamari.shrimp.ShrimpFractionExpressionInterface;
+import org.cirdles.calamari.tasks.expressions.ExpressionTreeInterface;
+
+/**
+ *
+ * @author James F. Bowring
+ */
+public class AgePb76 extends Function {
+
+    /**
+     * Provides the functionality of Squid's agePb76 by calling pbPbAge and
+     * returning "Age" and "AgeErr" and encoding the
+     * labels for each cell of the values array produced by eval2Array.
+     *
+     * @see
+     * https://raw.githubusercontent.com/CIRDLES/LudwigLibrary/master/vbaCode/isoplot3Basic/Pub.bas
+     * @see
+     * https://raw.githubusercontent.com/CIRDLES/LudwigLibrary/master/vbaCode/isoplot3Basic/UPb.bas
+     */
+    public AgePb76() {
+        name = "agePb76";
+        argumentCount = 1;
+        precedence = 4;
+        rowCount = 1;
+        colCount = 2;
+        labelsForValues = new String[][]{{"Age", "AgeErr"}};
+    }
+
+    /**
+     * Requires that child 0 is a VariableNode that evaluates
+     * to a double array with one row and a column for each member of
+     * shrimpFractions. 
+     *
+     * @param childrenET list containing child 0 through 3
+     * @param shrimpFractions a list of shrimpFractions
+     * @return the double[1][3] array of age, ageErr
+     */
+    @Override
+    public double[][] eval2Array(
+            List<ExpressionTreeInterface> childrenET, List<ShrimpFractionExpressionInterface> shrimpFractions) {
+
+        double[][] retVal;
+        try {
+            double pb207_206 = childrenET.get(0).eval2Array(shrimpFractions)[0][0];
+            double[] agePb76 = org.cirdles.ludwig.IsoplotUPb.pbPbAge(pb207_206)[0];          
+            retVal = new double[][]{{agePb76[0], agePb76[1]}};
+        } catch (ArithmeticException e) {
+            retVal = new double[][]{{0.0, 0.0}};
+        }
+
+        return retVal;
+    }
+
+    /**
+     *
+     * @param childrenET the value of childrenET
+     * @return
+     */
+    @Override
+    public String toStringMathML(List<ExpressionTreeInterface> childrenET) {
+        String retVal
+                = "<mrow>"
+                + "<mi>AgePb76</mi>"
+                + "<mfenced>";
+
+        for (int i = 0; i < childrenET.size(); i++) {
+            retVal += toStringAnotherExpression(childrenET.get(i)) + "&nbsp;\n";
+        }
+
+        retVal += "</mfenced></mrow>\n";
+
+        return retVal;
+    }
+
+}
